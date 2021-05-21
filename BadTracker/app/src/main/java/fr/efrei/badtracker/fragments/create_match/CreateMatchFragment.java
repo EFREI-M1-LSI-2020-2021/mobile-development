5ee74a33a941122bf5b39734b35728954bf6dbb0
Thomas LACAZE <contact@thomaslacaze.fr>
2021-05-21 12:59:29 +0200

    change date format with Gson

--- BadTracker/app/src/main/java/fr/efrei/badtracker/fragments/create_match/CreateMatchFragment.java
+++ BadTracker/app/src/main/java/fr/efrei/badtracker/fragments/create_match/CreateMatchFragment.java
@@ -19,6 +19,8 @@ import androidx.navigation.fragment.NavHostFragment;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializer;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -148,7 +150,7 @@ public class CreateMatchFragment extends Fragment {
             matchDao.add(match);
 
             Gson gson = new GsonBuilder()
-                    .registerTypeAdapter(Date.class, (JsonDeserializer<Date>) (json, typeOfT, context) -> new Date(json.getAsJsonPrimitive().getAsLong()))
+                    .registerTypeAdapter(Date.class, (JsonSerializer<Date>) (date, type, jsonSerializationContext) -> new JsonPrimitive(date.getTime()))
                     .create();
             System.out.println(gson.toJson(match));
 
